dbca4ee0ed36a1a25ea9928a452452ee4d2a0d03
Da Zheng <zhengda1936@gmail.com>
2017-03-14 08:34:24 -0400

    Update the script to generate API docs.

--- scripts/gen_doc.sh
+++ scripts/gen_doc.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-if [ $# -lt 1 ];
+if [ $# -lt 2 ];
 then
-	echo "gen_doc.sh man_dir"
+	echo "gen_doc.sh FlashR_man_dir FlashGraphR_man_dir"
 	exit 1
 fi
 
-mandir=$1
-echo $mandir
+FlashR_mandir=$1
+FlashGraphR_mandir=$2
+echo $FlashR_mandir
+echo $FlashGraphR_mandir
 
-echo "tools::Rdindex(\"$mandir\", \"/tmp/index.Rd\", width=200)" | R --no-save
-perl scripts/gen_index.pl /tmp/index.Rd pages/FlashR-API.md
+echo "tools::Rdindex(\"$FlashR_mandir\", \"/tmp/index.Rd\", width=200)" | R --no-save
+perl scripts/gen_index.pl /tmp/index.Rd pages/FlashR-API.md FlashR
+echo "tools::Rdindex(\"$FlashGraphR_mandir\", \"/tmp/index.Rd\", width=200)" | R --no-save
+perl scripts/gen_index.pl /tmp/index.Rd pages/FlashGraphR-API.md FlashGraphR
 mkdir -p FlashR-API
-for file in `ls $mandir`
+for file in `ls $FlashR_mandir`
 do
-	input_file="$mandir/$file"
+	input_file="$FlashR_mandir/$file"
 	output_file="FlashR-API/${file}.html"
+	echo "tools::Rd2HTML(\"$input_file\", out=\"$output_file\")" | R --no-save
+done
+
+mkdir -p FlashGraphR-API
+for file in `ls $FlashGraphR_mandir`
+do
+	input_file="$FlashGraphR_mandir/$file"
+	output_file="FlashGraphR-API/${file}.html"
 	echo "tools::Rd2HTML(\"$input_file\", out=\"$output_file\")" | R --no-save
 done
--- scripts/gen_index.pl
+++ scripts/gen_index.pl
@@ -5,21 +5,22 @@ use strict;
 my $num_args = @ARGV;
 
 if ($num_args < 2) {
-	print STDERR "gen_index rd_file html_file\n";
+	print STDERR "gen_index rd_file html_file proj_name\n";
 	exit(1);
 }
 
 open(my $in_fh, "<", $ARGV[0]) or die "open $ARGV[0]: $!";
 open(my $out_fh, ">", $ARGV[1]) or die "open $ARGV[1]: $!";
+my $proj_name = $ARGV[2];
 
 print $out_fh "---\n";
-print $out_fh "title: FlashR API\n";
+print $out_fh "title: $proj_name API\n";
 print $out_fh "keywords: API\n";
 print $out_fh "last_updated: Dec 28, 2016\n";
 print $out_fh "tags: [API]\n";
-print $out_fh "summary: \"FlashR API\"\n";
+print $out_fh "summary: \"$proj_name API\"\n";
 print $out_fh "sidebar: mydoc_sidebar\n";
-print $out_fh "permalink: FlashR-API.html\n";
+print $out_fh "permalink: $proj_name-API.html\n";
 print $out_fh "folder: mydoc\n";
 print $out_fh "---\n\n\n";
 print $out_fh "| name | comment |\n";
@@ -33,7 +34,7 @@ while (<$in_fh>) {
 	if ($_ =~ /([^ ]+) +([^ ].*)$/) {
 		my $name = $1;
 		my $msg = $2;
-		print $out_fh "| [$name](FlashR-API/${name}.Rd.html) | $msg |\n";
+		print $out_fh "| [$name]($proj_name-API/${name}.Rd.html) | $msg |\n";
 	}
 }
 
